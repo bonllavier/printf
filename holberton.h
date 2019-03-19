43ed6b19f1a34423c6db02c4b59461ea2432b360
javb92 <mentor37_@hotmail.com>
2019-03-19 21:01:41 +0000

    fix missing method

--- holberton.h
+++ holberton.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 int printchar(va_list str);
 int printstring(va_list str);
 int printdigit(va_list str);
+int (*get_op_func(char *s))(va_list str)
 int _printf(const char *format, ...);
 /**
  *struct op - structure
