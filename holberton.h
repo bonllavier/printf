456cf89ce04cf1bd1a1d8906da43228613ac71d3
javb92 <mentor37_@hotmail.com>
2019-03-19 21:28:54 +0000

    fixed coma

--- holberton.h
+++ holberton.h
@@ -5,13 +5,13 @@ int _putchar(char c);
 int printchar(va_list str);
 int printstring(va_list str);
 int printdigit(va_list str);
-int (*get_op_func(char *s))(va_list str)
+int (*get_op_func(char *s))(va_list str);
 int _printf(const char *format, ...);
-
 /**
  *struct op - structure
  *@op: character
  *@f: function name
+ *Description - selector for the proper operation.
  */
 typedef struct op
 {
